4a57f425cbbe034c52634582c67e447450d620c8
Raph <raffy211@gmail.com>
2022-11-22 13:55:29 -0800

    created a dark colour for the cancel and save button as well as created a dark theme function that adds the darker colour elements to the buttons when the dark theme button is clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
-const darkThemeButton = document.querySelector(".b2")
+const darkThemeButton = document.querySelector(".b2");
 
-const newNoteButton = document.querySelector(".b1")
+const newNoteButton = document.querySelector(".b1");
 
-const saveButton = document.querySelector(".b3")
+const saveButton = document.querySelector(".b3");
 
-const cancelButton = document.querySelector(".b4")
+const cancelButton = document.querySelector(".b4");
 
 function darkTheme(){
-    newNoteButton.classList.add("b1dark")
+    newNoteButton.classList.add("b1dark");
+    saveButton.classList.add("b3dark");
+    cancelButton.classList.add("b4dark");
+};
 
-}
+darkThemeButton.addEventListener("click", darkTheme);
