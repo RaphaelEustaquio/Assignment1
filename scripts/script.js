ec96f3153cfed242f4e4be5ff114e99765bcca26
Raph <raffy211@gmail.com>
2022-11-22 14:27:44 -0800

    added classes to some semantic tags in the css as well as updated the colours to toggle between light theme and dark theme whenver the button is clicked

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,27 @@ const saveButton = document.querySelector(".b3");
 
 const cancelButton = document.querySelector(".b4");
 
+const sideBar = document.querySelector(".sidebar");
+
+const textContainer = document.querySelector(".textcontainer")
+
 function darkTheme(){
-    newNoteButton.classList.add("b1dark");
-    saveButton.classList.add("b3dark");
-    cancelButton.classList.add("b4dark");
+    newNoteButton.classList.toggle("b1dark");
+    saveButton.classList.toggle("b3dark");
+    cancelButton.classList.toggle("b4dark");
+    sideBar.classList.toggle("sidebardark");
+    document.body.classList.toggle("textcontainerdark");
+};
+
+function changeName(){
+    if (darkThemeButton.textContent === "Dark Theme"){
+        darkThemeButton.textContent = "Light Theme"
+    }
+    else if (darkThemeButton.textContent === "Light Theme"){
+        darkThemeButton.textContent = "Dark Theme";
+    };
 };
 
 darkThemeButton.addEventListener("click", darkTheme);
+
+darkThemeButton.addEventListener("click", changeName);
