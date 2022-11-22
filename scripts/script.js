21a9884d60af3841900a4eafbf4c273a4a76d217
Raph <raffy211@gmail.com>
2022-11-22 13:50:19 -0800

    created variables for each button in js and added a darker colour for dark theme button and new note button

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const darkThemeButton = document.querySelector(".b2")
+
+const newNoteButton = document.querySelector(".b1")
+
+const saveButton = document.querySelector(".b3")
+
+const cancelButton = document.querySelector(".b4")
+
+function darkTheme(){
+    newNoteButton.classList.add("b1dark")
+
+}
